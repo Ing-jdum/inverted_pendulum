131111877e2e75c038af6f7fd27591c3f682d0bb
jdum <ing.jdum@gmail.com>
2026-03-16 15:58:33 -0400

    working values

--- test_LQR.ino
+++ test_LQR.ino
@@ -1,57 +1,5 @@
 #include <SimpleFOC.h>
-
-// ============================================================
-// HARDWARE MAPPING (STM32 NUCLEO)
-// ============================================================
-namespace Pins {
-    constexpr int MOTOR_PP      = 11;
-    constexpr int PWM_A         = 6;
-    constexpr int PWM_B         = 10;
-    constexpr int PWM_C         = 5;
-    constexpr int DRIVER_EN     = 8;
-    
-    constexpr int MOT_ENC_A     = 3;
-    constexpr int MOT_ENC_B     = 2;
-    
-    constexpr int PEND_ENC_A    = A4;
-    constexpr int PEND_ENC_B    = A5;
-    
-    constexpr int CURR_SENSE_A  = A0;
-    constexpr int CURR_SENSE_B  = A2;
-}
-
-// ============================================================
-// PHYSICS & CONTROL CONSTANTS
-// ============================================================
-namespace Config {
-    // Encoder
-    constexpr int ENC_CPR       = 1024;
-    
-    // Limits
-    constexpr float VOLTAGE_LIMIT = 12.0f;
-    constexpr float TORQUE_LIMIT  = 1.5f;
-    constexpr float MOTOR_SOFT_LIMIT_RAD = 1.8f * _PI;
-    constexpr float LQR_MOTOR_DEADBAND = 0.02f; 
-    constexpr float MIN_ACTUATION_TORQUE =  0.5f;
-    constexpr float TORQUE_ZERO_THRESHOLD = 0.02f;
-    constexpr float LQR_MOTOR_POS_DEADBAND = 0.1f;  // rad
-    constexpr float LQR_MOTOR_VEL_DEADBAND = 0.2f;  // rad/s
-    constexpr float LQR_PEND_POS_DEADBAND = 0.01f;  // rad
-    constexpr float LQR_PEND_VEL_DEADBAND = 1.0f;   // rad/s
-    
-    // Transitions (Hysteresis prevents "chattering" between states)
-    constexpr float STABILIZE_THRESHOLD = 0.25f; // rad
-    constexpr float FALL_THRESHOLD      = 0.25f; // rad
-    constexpr float RESET_POS_THRESHOLD = 0.05f; // rad
-    constexpr float RESET_VEL_THRESHOLD = 0.10f; // rad/s
-    
-    // LQR Gains (K = [pos_m, vel_m, pos_p, vel_p])
-    constexpr float K1 = -0.9165f, K2 = -0.66789f, K3 = 3.8111f, K4 = 0.09243f;
-    constexpr float RADIUS = 0.09;
-    
-    // Timing
-    constexpr unsigned long CONTROL_PERIOD_US = 1000; // 1kHz
-}
+#include <config.h>
 
 // ============================================================
 // OBJECT INSTANTIATION
@@ -103,7 +51,7 @@ float applyMinimumTorque(float u) {
 // ============================================================
 
 float runSwingUp(float p_angle, float p_vel) {
-    const float K = 0.0006f;
+    const float K = 0.0008f;
     // Energy based swing-up logic
     float energy_term = 9.81f * (1.0f - cosf(p_angle)) - 0.0075f * p_vel * p_vel;
     float tau = K * powf(cosf(p_angle), 4) * p_vel * energy_term;
@@ -262,7 +210,7 @@ void controlStep() {
 
     // Apply our minimum torque logic
     float final_target = applyMinimumTorque(raw_target);
-    motor.move(raw_target);
+    motor.move(final_target);
     target_torque_global = final_target;
 }
 
